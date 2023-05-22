58b69a74c735e236fd15fbe9090c1986c0a25df7
michelecanini <studio.michelecanini@gmail.com>
2023-05-22 18:04:01 +0200

    [UPDATE] create a list email

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//PROBLEMA 1: CHIEDI ALL'UTENTE LA SUA EMAIL, EFFETTUA UN CONTROLLO E STAMPA
+//L'ESITO ATTRAVERSO UN MESSAGGIO.
+
+let user_list = ['utente1email@lista.it', 'utente2email@lista.it', 'utente3email@lista.it', 'utente4email@lista.it', 'utente5email@lista.it', 'utente6email@lista.it', 'utente7email@lista.it', 'utente8email@lista.it', 'utente9email@lista.it', 'utente10email@lista.it']
+
+console.log(user_list)
+
+for(let i=0; i<user_list.length; i++){
+    console.log(user_list[i])
+}
