788e151ff85c9f520da6f49f3280e7e04ed5ee2f
michelecanini <studio.michelecanini@gmail.com>
2023-05-22 22:46:58 +0200

    [UPDATE] insert variable score end random -  floor

--- dadi_problem_two/js/script.js
+++ dadi_problem_two/js/script.js
@@ -2,4 +2,11 @@
 //PER IL COMPUTER, STABILISCI IL VINCITORE CHE FA IL PUNTEGGIO PIU ALTO.
 
 let button = document.getElementById('submit').addEventListener('click', function(){
+
 });
+
+let you_score = Math.random() * 6 + 1
+    console.log(Math.floor(you_score));
+
+let computer_score = Math.random() * 6 + 1
+    console.log(Math.floor(computer_score));
