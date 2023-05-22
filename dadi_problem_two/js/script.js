7bf79ab24637b161dcc7d680f550e648ccf81496
michelecanini <studio.michelecanini@gmail.com>
2023-05-23 01:38:29 +0200

    [UPDATE] esercize problem 2 done

--- dadi_problem_two/js/script.js
+++ dadi_problem_two/js/script.js
@@ -1,28 +1,26 @@
+"use strict";
+
 //PROBLEMA 2: GENERA UN NUMERO RANDOM DA 1 A 6 (DADI) SIA PER IL GIOCATORE CHE
 //PER IL COMPUTER, STABILISCI IL VINCITORE CHE FA IL PUNTEGGIO PIU ALTO.
 
 let button = document.getElementById('submit').addEventListener('click', function(){
 
-    let you_score = (Math.random() * 6) + 1;
+    let you_score = (Math.floor(Math.random() * 6) + 1);
+        document.getElementById('you_score').innerText = (you_score);
         console.log (Math.floor(you_score));
         
-    let computer_score = (Math.random() * 6) + 1;
+    let computer_score = (Math.floor(Math.random() * 6) + 1);
+        document.getElementById('computer_score').innerText = (computer_score);
         console.log(Math.floor(computer_score));
 
+    let message = "Un Pareggio! Ritenta la prossima volta sarai piu' fortunato";
+
     if(you_score > computer_score){
-        message = ('Complimenti hai Vinto');
-
-    } else if(you_score < computer_score){
-        message = ('Ci dispiace hai perso, il Computer questa volta ti ha battuto!');
-
-    } else if (you_score === computer_score);{
-        message = ("Un Pareggio! Ritenta la prossima volta sarai piu' fortunato");
-    }
-
-    document.getElementById('message').innerText = message;
-
+        message = ("Complimenti hai Vinto");
+    } 
+    else if (you_score < computer_score){
+        message = ("Ci dispiace hai perso, il Computer questa volta ti ha battuto!");
+    } 
+    
+    document.getElementById('message_print').innerText = message;
 });
-
-//document.getElementById('you_score').innerHTML += Math.floor(you_score);
-
-//document.getElementById('computer_score').innerHTML += Math.floor(computer_score);
